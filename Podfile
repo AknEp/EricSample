e4dc88b775fa43cdb5a1c61e3168e4fec32033c1
Satoshi SUZUKI <akn@s-satoshi.net>
2013-09-27 01:06:44 +0900

    CocoaPodsで各種ライブラリを導入

---
+++ Podfile
@@ -0,0 +1,6 @@
+platform :ios,'6.0'
+pod 'AFNetworking' , '~> 1.3.1'
+pod 'BlocksKit'
+pod 'SVProgressHUD'
+pod 'ActionSheetPicker2', '~> 0.1'
+
---
+++ Pods/Pods-environment.h
@@ -0,0 +1,38 @@
+
+// To check if a library is compiled with CocoaPods you
+// can use the `COCOAPODS` macro definition which is
+// defined in the xcconfigs so it is available in
+// headers also when they are imported in the client
+// project.
+
+
+// AFNetworking
+#define COCOAPODS_POD_AVAILABLE_AFNetworking
+#define COCOAPODS_VERSION_MAJOR_AFNetworking 1
+#define COCOAPODS_VERSION_MINOR_AFNetworking 3
+#define COCOAPODS_VERSION_PATCH_AFNetworking 2
+
+// ActionSheetPicker2
+#define COCOAPODS_POD_AVAILABLE_ActionSheetPicker2
+#define COCOAPODS_VERSION_MAJOR_ActionSheetPicker2 0
+#define COCOAPODS_VERSION_MINOR_ActionSheetPicker2 1
+#define COCOAPODS_VERSION_PATCH_ActionSheetPicker2 0
+
+// BlocksKit
+#define COCOAPODS_POD_AVAILABLE_BlocksKit
+#define COCOAPODS_VERSION_MAJOR_BlocksKit 1
+#define COCOAPODS_VERSION_MINOR_BlocksKit 8
+#define COCOAPODS_VERSION_PATCH_BlocksKit 3
+
+// SVProgressHUD
+#define COCOAPODS_POD_AVAILABLE_SVProgressHUD
+#define COCOAPODS_VERSION_MAJOR_SVProgressHUD 0
+#define COCOAPODS_VERSION_MINOR_SVProgressHUD 9
+#define COCOAPODS_VERSION_PATCH_SVProgressHUD 0
+
+// libffi
+#define COCOAPODS_POD_AVAILABLE_libffi
+#define COCOAPODS_VERSION_MAJOR_libffi 3
+#define COCOAPODS_VERSION_MINOR_libffi 0
+#define COCOAPODS_VERSION_PATCH_libffi 13
+
